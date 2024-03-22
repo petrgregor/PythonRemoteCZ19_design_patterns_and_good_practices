3a8a10ed07dace4b01268b517805b03d81ff5d77
Petr Gregor <petr.gregor@email.cz>
2024-03-22 17:56:23 +0100

    Task 1 - Singleton.

---
+++ 35_task_1_singleton.py
@@ -0,0 +1,3 @@
+"""Task 1 - Singleton
+Using the Singleton pattern, create a simple logger class that writes strings and the date and time to the file.
+Use a nested class that collects the instance functionality and the new method overload."""
